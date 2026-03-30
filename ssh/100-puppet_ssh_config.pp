558c458fa481dba2c8bad5359c2ed80a6dea4242
IREBE Asnath <b.asnath@alustudent.com>
2026-03-30 23:17:08 +0200

    Add puppet ssh config file

---
+++ ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# This Puppet manifest configures the SSH client
+include stdlib
+
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => '    IdentityFile ~/.ssh/school',
+}
+
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => '    PasswordAuthentication no',
+}
